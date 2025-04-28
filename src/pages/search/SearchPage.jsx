c35143feb914118b02a7528df9b881757afe16bd
apple <apple@apples-MacBook-Air.local>
2025-04-28 22:54:16 +0530

    completed search page except substitution details

--- src/pages/search/SearchPage.jsx
+++ src/pages/search/SearchPage.jsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { supabase } from "../../../utils/supabase";
 import { BackLogo } from "../../components/Logos";
+import { useNavigate } from "react-router-dom";
 
 function SearchPage() {
   const [searchTerm, setSearchTerm] = useState("");
   const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const searchForMatch = async () => {
+    console.log("loadin");
+    setLoading(true);
     try {
       const constantSearchTerm = searchTerm.toLowerCase();
       const { data, error } = await supabase.rpc("search_substitutions", {
@@ -16,20 +21,27 @@ function SearchPage() {
       if (error) {
         console.error("Error calling the function:", error);
         setResults({ error: "Error fetching data" });
+        setLoading(false);
       } else {
         if (data) {
+          console.log(data);
           const updated = updateMatchedFields(data, constantSearchTerm);
           setResults(updated);
+          setLoading(false);
         }
       }
     } catch (error) {
       console.error("Error:", error);
       setResults({ error: "Error fetching data" });
+      setLoading(false);
     }
   };
 
   const updateMatchedFields = (data, searchTerm) => {
-    if (!data || data.length === 0) return [];
+    if (!data || data.length === 0) {
+      console.log("no matches");
+      return [];
+    }
 
     return data.map((sub) => {
       const importantFields = [
@@ -58,9 +70,15 @@ function SearchPage() {
   };
 
   return (
-    <div className="fixed h-[100dvh] w-[100vw] bg-backgroundc">
+    <div className="fixed h-[100dvh] w-[100vw] bg-backgroundc overflow-scroll">
       <div className="h-[10dvh] w-full text-textc flex flex-row items-center gap-[10px] px-[min(3vw,50px)]">
-        <BackLogo size="min(3rem,50px)" />
+        <div
+          onClick={() => {
+            navigate("/home");
+          }}
+        >
+          <BackLogo size="min(3rem,50px)" />
+        </div>
         <span className="text-heading font-bold">Search</span>
       </div>
       <div className="p-[min(3vw,50px)]">
@@ -69,25 +87,20 @@ function SearchPage() {
             type="text"
             className="bg-textc outline-none w-[100%]"
             placeholder="Enter to search"
+            onChange={(e) => {
+              setSearchTerm(e.target.value);
+              searchForMatch();
+            }}
           ></input>
         </div>
       </div>
-      <input
-        type="text"
-        placeholder="Enter text"
-        className="text-textc"
-        onChange={(e) => {
-          setSearchTerm(e.target.value);
-        }}
-      ></input>
-      <button onClick={() => searchForMatch()} className="text-textc">
-        Search
-      </button>
-      <div className="flex flex-col gap-[10px] p-[min(3vw,50px)]">
-        {results && results.length > 0
-          ? results.map((sub, index) => (
+
+      <div className="flex flex-col gap-[10px] p-[min(3vw,50px)] w-[clamp(300px,100%,700px)]">
+        {!loading ? (
+          results && results.length > 0 ? (
+            results.map((sub, index) => (
               <div key={index} className="bg-backgroundh rounded-md">
-                <div className="p-[min(3vw,30px)] flex flex-row bg-textc text-backgroundc rounded-md">
+                <div className="p-[min(3vw,30px)] flex flex-row bg-textc text-backgroundc rounded-md ">
                   <div className="flex flex-col flex-1/2 justify-between">
                     <span>{sub.subject_name}</span>
                     <div className="flex flex-row text-[min(1rem,10px)]">
@@ -119,7 +132,16 @@ function SearchPage() {
                 )}
               </div>
             ))
-          : null}
+          ) : (
+            <div className="text-textc p-[min(3vw,50px)] font-light">
+              No substitutions found
+            </div>
+          )
+        ) : (
+          <div className="flex justify-center items-center py-10 text-textc">
+            <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-accentc"></div>
+          </div>
+        )}
       </div>
     </div>
   );
